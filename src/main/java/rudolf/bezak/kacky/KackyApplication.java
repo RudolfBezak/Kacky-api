46e79650ae55e889d39240489040a9c0a9300605
floduR <brezacik000@gmail.com>
2022-05-10 15:11:21 +0200

    commit

---
+++ src/main/java/rudolf/bezak/kacky/KackyApplication.java
@@ -0,0 +1,15 @@
+package rudolf.bezak.kacky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KackyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KackyApplication.class, args);
+		System.out.println("ahoj rudolf");
+
+	}
+
+}
